15b521f3f28c7b1717835db73af7c9161a25b8bc
seonyong-kim <kimsy7836@naver.com>
2025-05-02 02:57:39 +0900

    2장

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 40, fontWeight: "bold" }}>메인 화면</Text>
+    </View>
+  );
+};
+
+const DetailScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 40, fontWeight: "bold" }}>상세보기 화면</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Details">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
