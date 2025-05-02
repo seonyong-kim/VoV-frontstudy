27b9bec7ddff5e504f66454077796999473ebee3
seonyong-kim <kimsy7836@naver.com>
2025-05-02 11:38:46 +0900

    3강

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <Text style={{ fontSize: 40, fontWeight: "bold" }}>메인 화면</Text>
+      <Button
+        title="상세 페이지 이동"
+        onPress={() => navigation.navigate("Details")} 
+      />
     </View>
   );
 };
 
-const DetailScreen = () => {
+const DetailScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
       <Text style={{ fontSize: 40, fontWeight: "bold" }}>상세보기 화면</Text>
+      <Button title="홈 화면 이동" onPress={() => navigation.navigate("Home")} />
+      <Button title="상세 페이지로 이동" onPress={() => navigation.push("Details")} />
       <StatusBar style="auto" />
     </View>
   );
@@ -25,7 +32,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Details">
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailScreen} />
       </Stack.Navigator>
