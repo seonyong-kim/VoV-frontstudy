12098e3759d4fc125d69d0af74eeee35274aa176
seonyong-kim <kimsy7836@naver.com>
2025-05-08 01:40:59 +0900

    할 일 작성 UI 변경

--- screens/TodoWriteScreen.js
+++ screens/TodoWriteScreen.js
@@ -1,4 +1,4 @@
-import { Text, View, TextInput, Pressable } from "react-native";
+import { Text, View, TextInput, Pressable, StyleSheet } from "react-native";
 import React, { useState } from "react";
 
 const TodoWriteScreen = ({ navigation }) => {
@@ -11,38 +11,63 @@ const TodoWriteScreen = ({ navigation }) => {
         onChangeText={setTodo}
         value={todo}
         placeholder="할 일을 작성해주세요."
-        style={{
-          flex: 0.3,
-          padding: 10,
-          backgroundColor: "#fff",
-          borderRadius: 10,
-          borderWidth: 2,
-          margin: 10,
-        }}
+        style={styles.inputBox}
       />
-      <Pressable
-        onPress={() => {
-          navigation.navigate("Details", { todo });
-          setTodo("");
+      <View
+        style={{
+          flexDirection: "row",
+          gap: 5,
+          justifyContent: "center",
+          marginLeft: 10,
+          marginRight: 10,
         }}
       >
-        <Text
-          style={{
-            padding: 10,
-            backgroundColor: "#fff",
-            borderRadius: 10,
-            borderWidth: 2,
-            width: "30%",
-            textAlign: "center",
-            fontWeight: "bold",
-            margin: 10,
+        <Pressable
+          style={styles.pressableBtn}
+          onPress={() => {
+            navigation.navigate("TodoList", { todo });
+            setTodo("");
           }}
         >
-          작성
-        </Text>
-      </Pressable>
+          <Text style={styles.text}>작성</Text>
+        </Pressable>
+        <Pressable
+          style={styles.pressableBtn}
+          onPress={() => {
+            navigation.navigate("Home");
+          }}
+        >
+          <Text style={styles.text}>취소</Text>
+        </Pressable>
+      </View>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  inputBox: {
+    minHeight: 200,
+    padding: 10,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    borderWidth: 2,
+    margin: 10,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  pressableBtn: {
+    width: "50%",
+    padding: 10,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    borderWidth: 2,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
 
 export default TodoWriteScreen;
